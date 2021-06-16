640af1aabcd44da6f513426c2a4a7ee27a93702e
Ya Sun <ya.sun@dianrong.com>
2021-06-16 22:10:46 +0800

    update exit

--- run.sh
+++ run.sh
@@ -1,81 +1,41 @@
 #!/bin/bash
-#签到
-node jd_bean_sign.js
-#东东超市兑换奖品 #摇京豆 #京东汽车兑换 #家电星推官 #家电星推官好友互助
-node jd_blueCoin.js
-node jd_club_lottery.js
-node jd_car_exchange.js
-node jd_xtg.js
-node jd_xtg_help.js
-
-#东东农场 #东东萌宠 #口袋书店 #京喜农场 #京东极速版签到 #京东家庭号 #金榜创造营 #明星小店
-node jd_fruit.js
-node jd_pet.js
-node jd_bookshop.js
-node jd_jxnc.js
-node jd_speed_sign.js
-node jd_family.js
-node jd_gold_creator.js
-node jd_star_shop.js
-
-#宠汪汪喂食 #宠汪汪 #摇钱树 #京东种豆得豆 #京喜工厂 #东东工厂 #东东健康社区收集能量
-node jd_joy_feedPets.js
-node jd_joy.js
-node jd_moneyTree.js
-node jd_plantBean.js
-node jd_dreamFactory.js
-node jd_jdfactory.js
-node jd_health_collect.js
-
-#宠汪汪积分兑换京豆 #签到领现金 #点点券 #东东小窝 #京喜财富岛 #京东直播 #东东健康社区 #每日抽奖 #女装魔盒 #跳跳乐 #5G超级盲盒
-node jd_joy_reward.js
-node jd_cash.js
-node jd_necklace.js
-node jd_small_home.js
-node jd_cfd.js
-node jd_live.js
-node jd_health.js
-node jd_daily_lottery.js
-node jd_nzmh.js
-node jd_jump.js
-node jd_mohe.js
-
-#京东全民开红包 #进店领豆 #取关京东店铺商品 #京东抽奖机 #京东汽车 #京东秒秒币
-node jd_redPacket.js
-node jd_shop.js
-node jd_unsubscribe.js
-node jd_lotteryMachine.js
-node jd_car.js
-node jd_ms.js
-
-#天天提鹅 #手机狂欢城
-node jd_daily_egg.js
-node jd_carnivalcity.js
-
-东东超市 #十元街 #动物联萌 #翻翻乐
-node jd_superMarket.js
-node jd_syj.js
-node jd_zoo.js
-node jd_big_winner.js
-
- #疯狂的joy #监控crazyJoy分红 #京东排行榜 #领京豆额外奖励 #京东保价 #闪购盲盒 #新潮品牌狂欢 #京喜领88元红包
-node jd_crazy_joy.js
-node jd_crazy_joy_bonus.js
-node jd_rankingList.js
-node jd_bean_home.js
-node jd_price.js
-node jd_sgmh.js
-node jd_mcxhd.js
-node jd_jxlhb.js
-
-#金融养猪 #京东快递 #京东赚赚 #京东极速版红包 #领金贴
-node jd_pigPet.js
-node jd_kd.js
-node jd_jdzz.js
-node jd_speed_redpocke.js
-node jd_jin_tie.js
-
-
-#京豆变动通知
+node  jd_bean_change.js
+node  jd_bean_home.js
+node  jd_bean_sign.js
+node  jd_blueCoin.js
+node  jd_bookshop.js
+node  jd_car.js
+node  jd_car_exchange.js
+node  jd_cash.js
+node  jd_club_lottery.js
+#node  jd_crazy_joy.js
+#node  jd_crazy_joy_coin.js
+node  jd_daily_egg.js
+node  jd_dreamFactory.js
+node  jd_family.js
+node  jd_fruit.js
+node  jd_get_share_code.js
+node  jd_jdfactory.js
+node  jd_jdzz.js
+node  jd_jxnc.js
+node  jd_kd.js
+node  jd_live.js
+node  jd_lotteryMachine.js
+node  jd_moneyTree.js
+node  jd_necklace.js
+node  jd_nh.js
+node  jd_pet.js
+#node  jd_petTreasureBox.js
+node  jd_pigPet.js
+node  jd_plantBean.js
+node  jd_rankingList.js
+node  jd_redPacket.js
+node  jd_shop.js
+node  jd_small_home.js
+node  jd_speed.js
+node  jd_superMarket.js
+node  jd_syj.js
+node  jd_unbind.js
+node  jd_unsubscribe.js
 node  jd_bean_change.js
 
