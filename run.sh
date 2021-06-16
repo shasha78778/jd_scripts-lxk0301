52ca24ab1f61b084e6d2c7ce96d51a46ad26fe97
Ya Sun <ya.sun@dianrong.com>
2021-06-16 22:26:58 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,41 +1,82 @@
-#!/bin/bash
-node  jd_bean_change.js
-node  jd_bean_home.js
-node  jd_bean_sign.js
-node  jd_blueCoin.js
-node  jd_bookshop.js
-node  jd_car.js
-node  jd_car_exchange.js
-node  jd_cash.js
-node  jd_club_lottery.js
-#node  jd_crazy_joy.js
-#node  jd_crazy_joy_coin.js
-node  jd_daily_egg.js
-node  jd_dreamFactory.js
-node  jd_family.js
-node  jd_fruit.js
-node  jd_get_share_code.js
-node  jd_jdfactory.js
-node  jd_jdzz.js
-node  jd_jxnc.js
-node  jd_kd.js
-node  jd_live.js
-node  jd_lotteryMachine.js
-node  jd_moneyTree.js
-node  jd_necklace.js
-node  jd_nh.js
-node  jd_pet.js
-#node  jd_petTreasureBox.js
-node  jd_pigPet.js
-node  jd_plantBean.js
-node  jd_rankingList.js
-node  jd_redPacket.js
-node  jd_shop.js
-node  jd_small_home.js
-node  jd_speed.js
-node  jd_superMarket.js
-node  jd_syj.js
-node  jd_unbind.js
-node  jd_unsubscribe.js
-node  jd_bean_change.js
+91
 
+#!/bin/bash
+#签到
+node jd_bean_sign.js || true
+#东东超市兑换奖品 #摇京豆 #京东汽车兑换 #家电星推官 #家电星推官好友互助
+node jd_blueCoin.js || true
+node jd_club_lottery.js || true
+node jd_car_exchange.js || true
+node jd_xtg.js || true
+node jd_xtg_help.js || true
+
+#东东农场 #东东萌宠 #口袋书店 #京喜农场 #京东极速版签到 #京东家庭号 #金榜创造营 #明星小店
+node jd_fruit.js || true
+node jd_pet.js || true
+node jd_bookshop.js || true
+node jd_jxnc.js || true
+node jd_speed_sign.js || true
+node jd_family.js || true
+node jd_gold_creator.js || true
+node jd_star_shop.js || true
+
+#宠汪汪喂食 #宠汪汪 #摇钱树 #京东种豆得豆 #京喜工厂 #东东工厂 #东东健康社区收集能量
+node jd_joy_feedPets.js || true
+node jd_joy.js || true
+node jd_moneyTree.js || true
+node jd_plantBean.js || true
+node jd_dreamFactory.js || true
+node jd_jdfactory.js || true
+node jd_health_collect.js || true
+
+#宠汪汪积分兑换京豆 #签到领现金 #点点券 #东东小窝 #京喜财富岛 #京东直播 #东东健康社区 #每日抽奖 #女装魔盒 #跳跳乐 #5G超级盲盒
+node jd_joy_reward.js || true
+node jd_cash.js || true
+node jd_necklace.js || true
+node jd_small_home.js || true
+node jd_cfd.js || true
+node jd_live.js || true
+node jd_health.js || true
+node jd_daily_lottery.js || true
+node jd_nzmh.js || true
+node jd_jump.js || true
+node jd_mohe.js || true
+
+#京东全民开红包 #进店领豆 #取关京东店铺商品 #京东抽奖机 #京东汽车 #京东秒秒币
+node jd_redPacket.js || true
+node jd_shop.js || true
+node jd_unsubscribe.js || true
+node jd_lotteryMachine.js || true
+node jd_car.js || true
+node jd_ms.js || true
+
+#天天提鹅 #手机狂欢城
+node jd_daily_egg.js || true
+node jd_carnivalcity.js || true
+
+东东超市 #十元街 #动物联萌 #翻翻乐
+node jd_superMarket.js || true
+node jd_syj.js || true
+node jd_zoo.js || true
+node jd_big_winner.js || true
+
+ #疯狂的joy #监控crazyJoy分红 #京东排行榜 #领京豆额外奖励 #京东保价 #闪购盲盒 #新潮品牌狂欢 #京喜领88元红包
+node jd_crazy_joy.js || true
+node jd_crazy_joy_bonus.js || true
+node jd_rankingList.js || true
+node jd_bean_home.js || true
+node jd_price.js || true
+node jd_sgmh.js || true
+node jd_mcxhd.js || true
+node jd_jxlhb.js || true
+
+#金融养猪 #京东快递 #京东赚赚 #京东极速版红包 #领金贴
+node jd_pigPet.js || true
+node jd_kd.js || true
+node jd_jdzz.js || true
+node jd_speed_redpocke.js || true
+node jd_jin_tie.js || true
+
+
+#京豆变动通知
+node  jd_bean_change.js || true
