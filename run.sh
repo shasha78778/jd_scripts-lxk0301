24623bf9a1f4947ff16eff383e2516c23cbe3eeb
SHOUGOUGOU <ya.sun.chn@gmail.com>
2021-06-22 16:36:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ node jd_xtg_help.js || true
 #东东农场 #东东萌宠 #口袋书店 #京喜农场 #京东极速版签到 #京东家庭号 #金榜创造营 #明星小店
 node jd_fruit.js || true
 node jd_pet.js || true
-node jd_bookshop.js || true
+#node jd_bookshop.js || true
 node jd_jxnc.js || true
 node jd_speed_sign.js || true
 node jd_family.js || true
@@ -21,16 +21,16 @@ node jd_gold_creator.js || true
 node jd_star_shop.js || true
 
 #宠汪汪喂食 #宠汪汪 #摇钱树 #京东种豆得豆 #京喜工厂 #东东工厂 #东东健康社区收集能量
-node jd_joy_feedPets.js || true
-node jd_joy.js || true
+#node jd_joy_feedPets.js || true
+#node jd_joy.js || true
 node jd_moneyTree.js || true
 node jd_plantBean.js || true
-node jd_dreamFactory.js || true
+#node jd_dreamFactory.js || true
 node jd_jdfactory.js || true
 node jd_health_collect.js || true
 
 #宠汪汪积分兑换京豆 #签到领现金 #点点券 #东东小窝 #京喜财富岛 #京东直播 #东东健康社区 #每日抽奖 #女装魔盒 #跳跳乐 #5G超级盲盒
-node jd_joy_reward.js || true
+#node jd_joy_reward.js || true
 node jd_cash.js || true
 node jd_necklace.js || true
 node jd_small_home.js || true
@@ -38,8 +38,8 @@ node jd_cfd.js || true
 node jd_live.js || true
 node jd_health.js || true
 node jd_daily_lottery.js || true
-node jd_nzmh.js || true
-node jd_jump.js || true
+#node jd_nzmh.js || true
+#node jd_jump.js || true
 node jd_mohe.js || true
 
 #京东全民开红包 #进店领豆 #取关京东店铺商品 #京东抽奖机 #京东汽车 #京东秒秒币
@@ -47,7 +47,7 @@ node jd_redPacket.js || true
 node jd_shop.js || true
 node jd_unsubscribe.js || true
 node jd_lotteryMachine.js || true
-node jd_car.js || true
+#node jd_car.js || true
 node jd_ms.js || true
 
 #天天提鹅 #手机狂欢城
@@ -55,8 +55,8 @@ node jd_daily_egg.js || true
 node jd_carnivalcity.js || true
 
 东东超市 #十元街 #动物联萌 #翻翻乐
-node jd_superMarket.js || true
-node jd_syj.js || true
+#node jd_superMarket.js || true
+#node jd_syj.js || true
 node jd_zoo.js || true
 node jd_big_winner.js || true
 
@@ -71,7 +71,7 @@ node jd_mcxhd.js || true
 node jd_jxlhb.js || true
 
 #金融养猪 #京东快递 #京东赚赚 #京东极速版红包 #领金贴
-node jd_pigPet.js || true
+#node jd_pigPet.js || true
 node jd_kd.js || true
 node jd_jdzz.js || true
 node jd_speed_redpocke.js || true
